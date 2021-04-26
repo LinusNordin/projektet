5964c851655fe237415d727dd997c0148bce838e
LinusNordin <linus.nordin@edu.nykoping.se>
2021-04-26 15:18:59 +0200

    mer layout bullshit

--- nav.js
+++ nav.js
@@ -2,16 +2,19 @@
 
 let nav = document.getElementById('nav')
 let x = document.getElementById('x')
+let main = document.getElementById('mainbody')
 
 //console.log(nav, x)
 
 x.addEventListener('click', function(){
     //console.log('Klickad på :)');
     nav.classList.toggle('open')
+    main.classList.toggle('bodyopen')
 })
 
 document.addEventListener('keydown', function(event) {
     if (event.key === "Escape" && nav.classList.contains('open')) {
         nav.classList.toggle('open')
+        main.classList.toggle('bodyopen')
     }
 })
